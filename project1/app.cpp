1b699564f09ebcaa9f9c729e01a22393132d2323
Andrei <isvoranandrei96@gmail.com>
2018-11-07 08:58:06 +0200

    good commit

---
+++ project1/app.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "project2//testl.h"
+
+int main()
+{
+hello();
+}
---
+++ project1/project2/testl.cpp
@@ -0,0 +1,8 @@
+#include "testl.h"
+
+using namespace std;
+
+void hello()
+{
+	std::cout<<"Hello"<<std::endl;
+}
