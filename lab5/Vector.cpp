d731f945550517987be07dd8913e9bbfc72d55b1
Andrei <isvoranandrei96@gmail.com>
2018-11-16 14:43:09 +0200

    Vector class

---
+++ lab5/Vector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+const int SIZE = 3;
+class atype {
+int a[SIZE];
+public:
+    atype() {
+        register int i;
+        for(i=0; i<SIZE; i++) a[i] = i;
+}
+int operator[](int i) {
+    if(i>SIZE || i < 0){
+        cout << "OutOfBounds\n";
+        exit(0);
+    }
+    return a[i];}
+};
+int main()
+{
+    atype ob;
+    cout << ob[2] << endl; // displays 2
+    cout << ob[5] << endl; // displays error
+    return 0;
+}
